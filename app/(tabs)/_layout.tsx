fd34f5f5bd9301bc5bfc248d8818dbb97420e1ab
genghiskhang <vinhkhl1@umbc.edu>
2025-05-01 17:25:10 -0400

    cleanup

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,24 +1,3 @@
-// import { Tabs } from 'expo-router';
-// import { useAuth } from '../../context/AuthContext';
-
-// export default function TabsLayout() {
-//   const { role } = useAuth();
-
-//   return (
-//     <Tabs>
-//       <Tabs.Screen name="index" options={{ title: 'Home' }} />
-//       <Tabs.Screen name="schedule" options={{ title: 'Schedule' }} />
-//       {/* {(role === 'admin' || true) && (
-//         <Tabs.Screen
-//           name="../(admin)/scan"
-//           options={{ title: 'Scan' }}
-//         />
-//       )} */}
-//       <Tabs.Screen name='../(admin)/scan' options={{ title: 'Scan' }} />
-//     </Tabs>
-//   );
-// }
-
 import { Tabs } from 'expo-router';
 import { useAuth } from '../../context/AuthContext';
 import { Button } from 'react-native';
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,13 +1,15 @@
 import { View, Text, Button } from 'react-native';
 import { useRouter } from 'expo-router';
+import { useAuth } from '../../context/AuthContext';
 
 export default function HomeScreen() {
+  const { role } = useAuth();
   const router = useRouter();
 
   return (
     <View style={{ padding: 20 }}>
       <Text style={{ fontSize: 24 }}>Home Page</Text>
-      <Button title="Go to Login" onPress={() => router.push('/login')} />
+      {!role && <Button title="Go to Login" onPress={() => router.push('/login')} />}
     </View>
   );
 }
